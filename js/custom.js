76b1a11a1b5a2bc10a72596441a375b51a8d6dfb
ZeljanZ <zivkov.zeljan@gmail.com>
2021-05-31 21:15:11 +0200

    setup-proud
    setup prude imange done
    need to finish overly click

--- js/custom.js
+++ js/custom.js
@@ -3,7 +3,7 @@ jQuery(document).ready(function($){
     setTimeout(function(){
         $(".loader-wrapper").fadeOut();
     },1000);
-
+    // search wrraper
     $("#search-btn").click(function(){
 
         $(".search-wrapper").fadeIn();
@@ -13,8 +13,20 @@ jQuery(document).ready(function($){
 
         $(".search-wrapper").fadeOut();
     });
+    
+    //proud wrapper
+    $("#item-6").click(function(){
+
+        $(".proud-wrapper").fadeIn();
+    });
+
+    $("#close-proud").click(function(){
+
+        $(".proud-wrapper").fadeOut();
+    });
 
 
+    //hamburger-menu
     $("#hamburger-btn").click(function(){
 
         if($(this).hasClass('opened')) {
